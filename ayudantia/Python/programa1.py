1a731a4f3cd33255e7e0245f4dcaa13c139176e9
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2022-01-18 09:00:59 -0600

    clase 18 Enero

--- ayudantia/Python/programa1.py
+++ ayudantia/Python/programa1.py
@@ -34,3 +34,31 @@ else:
 s3a = 3e0*3.65e2*2.4e1*3.6e3
 
 print(type(2 + 5j))
+
+
+
+a = 23
+b = "32"
+if( (a == b) or (a == a and b == b) ):
+	print("a")
+else :
+	print("b")
+
+
+print(1 + 3 > 2 + 5)
+
+
+#Distancia entre dos puntos
+x1 = 3
+x2 = 4
+y1 =  5
+y2 = 9
+
+def distancia(x1,x2,y1,y2):
+	a = (x2 -x1)**2
+	b = (y2- y1)**2
+	c = math.sqrt(a+b)
+	return str(c)
+
+
+print(distancia(x1,x2,y1,y2))
